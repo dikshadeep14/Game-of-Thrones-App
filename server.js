9d6528d45ce21042fed3081467e225812c5b1877
diksha deep <diksha.deep@mail.vinove.com>
2020-05-14 19:36:37 +0530

    add port

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const http = require('http');
+// const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 8080;
+// const port = process.env.PORT || 8080;
 
-const server = http.createServer(app);
-// var server = app.listen(process.env.PORT, function () {
-//     var port = server.address().port;
+// const server = http.createServer(app);
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
     console.log("App now running on port", port);
-  // });
-server.listen(port);
+  });
+// server.listen(port);
